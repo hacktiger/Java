605be9f83d30ec2abac4bbb9f06253979b7a790c
hacktiger <hacktiegr1989@gmail.com>
2018-07-09 09:08:19 +0700

    overhaul

---
+++ KnightTour/KnightTour.java
@@ -0,0 +1,16 @@
+package knighttour;
+
+/**
+ *
+ * @author Dell
+ */
+public class KnightTour {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
